bd43face547dda78cac770bf8ef53d7ffc56c3a3
KiHoonAhn1 <dksdmlwjd1@naver.com>
2020-10-22 01:39:32 +0900

    집에서 해본거+수업

---
+++ p701/app/src/main/java/com/example/p701/MainActivity.java
@@ -0,0 +1,97 @@
+package com.example.p701;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    NotificationManager manager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void ck1(View v){
+        Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        if(Build.VERSION.SDK_INT >= 26){ //버전 체크를 해줘야 작동하도록 한다
+            vibrator.vibrate(VibrationEffect.createOneShot(1000,10));
+        }else{
+            vibrator.vibrate(1000);
+        }
+    }
+    public void ck2(View v){
+        Uri uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        Ringtone ringtone = RingtoneManager.getRingtone(getApplicationContext(), uri);
+        ringtone.play();
+    }
+    public void ck3(View v){
+        MediaPlayer player = MediaPlayer.create(getApplicationContext(), R.raw.mp);
+        if(!player.isPlaying()){
+            player.start();
+        }else{
+            player.pause();
+        }
+    }
+    public void ck4(View v){
+        manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        NotificationCompat.Builder builder = null;
+        if(Build.VERSION.SDK_INT >= 26){
+            if(manager.getNotificationChannel("ch1") == null){
+                manager.createNotificationChannel(new NotificationChannel("ch1", "chname", NotificationManager.IMPORTANCE_HIGH));
+            }
+            builder = new NotificationCompat.Builder(this, "ch1");
+        }else{
+            builder = new NotificationCompat.Builder(this);
+        }
+        builder.setContentTitle("Noti Test");
+        builder.setContentText("Content Text");
+        builder.setSmallIcon(R.drawable.img02);
+        Notification noti = builder.build();
+        manager.notify(1,noti);
+    }
+    public void ck5(View v){
+        PendingIntent fullScreenPendingIntent;
+        manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        NotificationCompat.Builder builder = null;
+        if(Build.VERSION.SDK_INT >= 26){
+            if(manager.getNotificationChannel("ch2") == null){
+                manager.createNotificationChannel(new NotificationChannel("ch2", "chname", NotificationManager.IMPORTANCE_HIGH));
+            }
+            builder = new NotificationCompat.Builder(this, "ch2");
+        }else{
+            builder = new NotificationCompat.Builder(this);
+        }
+        Intent intent = new Intent(this, MainActivity.class);
+        builder.setPriority(Notification.PRIORITY_MAX);
+        fullScreenPendingIntent = PendingIntent.getActivity(
+                this, 101, intent, PendingIntent.FLAG_CANCEL_CURRENT
+        );
+        intent.setClass(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        builder.setFullScreenIntent(fullScreenPendingIntent, true);
+        builder.setWhen(System.currentTimeMillis());
+        builder.setAutoCancel(true);
+        builder.setContentIntent(fullScreenPendingIntent);
+        builder.setContentTitle("Noti Test");
+        builder.setContentText("Content Text");
+        builder.setSmallIcon(R.drawable.img02);
+        Notification noti = builder.build();
+        manager.notify(1,noti);
+    }
+}
---
+++ p712/app/src/main/java/com/example/p712/MainActivity.java
@@ -0,0 +1,92 @@
+package com.example.p712;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tx;
+    Intent intent;
+    NotificationManager manager;
+    PendingIntent fullScreenPendingIntent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tx = findViewById(R.id.tx);
+        FirebaseMessaging.getInstance().subscribeToTopic("car")
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                String msg = "FCM Complete ...";
+                if(!task.isSuccessful()){
+                    msg = "FCM Fail";
+                }
+                Log.d("[TAG]:",msg);
+            }
+        }); // 제목
+        LocalBroadcastManager lbm = LocalBroadcastManager.getInstance(this);
+        lbm.registerReceiver(receiver, new IntentFilter("notification"));
+    }//end onCreate
+
+    public BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if(intent != null){
+                String title = intent.getStringExtra("title");
+                String control = intent.getStringExtra("control");
+                String data = intent.getStringExtra("data");
+                tx.setText(control+" "+data);
+                Toast.makeText(MainActivity.this,
+                        title+" "+control+" "+data, Toast.LENGTH_LONG).show();
+            }
+            Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+            if(Build.VERSION.SDK_INT >= 26){ //버전 체크를 해줘야 작동하도록 한다
+                vibrator.vibrate(VibrationEffect.createOneShot(1000,20));
+            }else{
+                vibrator.vibrate(1000);
+            }
+            manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            NotificationCompat.Builder builder = null;
+            if(Build.VERSION.SDK_INT >= 26){
+                if(manager.getNotificationChannel("ch1") == null){
+                    manager.createNotificationChannel(new NotificationChannel("ch1", "chname", NotificationManager.IMPORTANCE_DEFAULT));
+                }
+                builder = new NotificationCompat.Builder(MainActivity.this, "ch1");
+            }else{
+                builder = new NotificationCompat.Builder(MainActivity.this);
+            }
+            builder.setContentTitle("Noti Test");
+            builder.setContentText("Content Text");
+            builder.setSmallIcon(R.drawable.img07);
+            Notification noti = builder.build();
+            manager.notify(1,noti);
+
+    }
+        };
+    } // MyFService의 intent가 보낸걸 얘가 받는다
